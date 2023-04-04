6441921585f6b2a7d2cf9a90c9adbe2f9df13126
Sewon <Sewon@sewonpark.com>
2023-04-04 18:40:19 +0900

    removed wrong lemma again

--- formalization/clerical/Nondeterminism.v
+++ formalization/clerical/Nondeterminism.v
@@ -1662,6 +1662,65 @@ Section PowerdomainContinuity.
     simpl.
     exists (total x); split; auto.
   Defined.
+
+  Lemma pdom_lift_empty_1 {X Y : Type} (f : X -> Y) (S : pdom X) :
+    (pdom_is_empty S) -> pdom_is_empty (pdom_lift f S).
+  Proof.
+    intros p x e.
+    unfold pdom_is_empty in p.
+    destruct e.
+    destruct H.
+    apply (p _ H).
+  Defined.
+
+  Lemma pdom_lift_total_1 {X Y : Type} (f : X -> Y) (S : pdom X) :
+    forall y, (exists x, total x ∈ S /\ y = f x) -> total y ∈ pdom_lift f S.
+  Proof.
+    intros.
+    destruct H.
+    destruct H.
+    rewrite H0.
+    simpl.
+    exists (total x).
+    simpl; auto.
+  Defined.
+    
+  Lemma pdom_lift_total_2 {X Y : Type} (f : X -> Y) (S : pdom X) :
+    forall y,  total y ∈ pdom_lift f S -> (exists x, total x ∈ S /\ y = f x) .
+  Proof.
+    intros.
+    simpl in H.
+    destruct H.
+    destruct x.
+    destruct H.
+
+    simpl in H0.
+    contradict (flat_bot_neq_total _ H0).
+    exists x.
+    simpl in H.
+    destruct H; split; auto.
+    injection H0; intro; auto.
+  Defined.
+
+  Lemma pdom_lift_bot_1 {X Y : Type} (f : X -> Y) (S : pdom X) :
+    (bot X ∈ S) -> bot Y ∈ pdom_lift f S.
+  Proof.
+    intros.
+    simpl.
+    exists (bot X); split; simpl; auto.
+  Defined.
+
+  Lemma pdom_lift_bot_2 {X Y : Type} (f : X -> Y) (S : pdom X) :
+    bot Y ∈ pdom_lift f S -> (bot X ∈ S) .
+  Proof.
+    intros.
+    destruct H.
+    destruct H.
+    simpl in H0.
+    destruct x; auto.
+    simpl in H0.
+    contradict (flat_total_neq_bot _ H0).
+  Defined.  
   
   Lemma pdom_bind_total_1 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
     forall x, (~ pdom_is_empty (pdom_bind f S) /\ exists s, (total s) ∈ S /\ x ∈ f s) -> x ∈ pdom_bind f S.
@@ -1715,38 +1774,206 @@ Section PowerdomainContinuity.
     rewrite H1.
     exact b.
   Defined.    
-    
-  Lemma pdom_bind_bot_1 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
-    ~ pdom_is_empty (pdom_bind f S) ->
-    (bot X ∈ S \/ exists x, total x ∈ S /\ bot Y ∈ f x) ->  
-    (bot Y) ∈ (pdom_bind f S).
-  Proof.
 
-    
-  Admitted.
-
-  Lemma pdom_bind_bot_2 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
-    (bot Y) ∈ (pdom_bind f S) -> bot X ∈ S \/ exists x, total x ∈ S /\ bot Y ∈ f x.
-  Proof.
-  Admitted.
-  
-  Lemma pdom_bind_not_contain_bot {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
-    ~ ((bot Y) ∈ pdom_bind f S) -> forall s, total s ∈ S -> ~ (bot Y ∈ f s).
-  Proof.
-  Admitted.
-  
   Lemma pdom_bind_empty_1 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
     pdom_is_empty S \/ (exists x, (total x) ∈ S /\ pdom_is_empty (f x)) -> pdom_is_empty (pdom_bind f S).
   Proof.
     intros.
-  Admitted.
-
+    destruct H.
+    intros x e.
+    destruct e.
+    apply pdom_nempty_neg_is_empty in H0.
+    apply H0, pdom_lift_empty_1, H.
+    intros x e.
+    destruct e as [a [b c]].
+    destruct H.
+    pose proof (b (f x0)).
+    assert (total (f x0) ∈ pdom_lift f S).
+    simpl.
+    exists (total x0); destruct H; split; auto.
+    apply H0 in H1.
+    apply pdom_nempty_neg_is_empty in H1.
+    destruct H; apply H1; auto.
+  Defined.
+  
   Lemma pdom_bind_empty_2 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
     pdom_is_empty (pdom_bind f S) ->
     pdom_is_empty S \/ (exists x, (total x) ∈ S /\ pdom_is_empty (f x)).
   Proof.
     intros.
-  Admitted.
+    destruct (lem (exists x : X, (total x ∈ S) /\ pdom_is_empty (f x))); auto.
+    left.
+    intros x e.
+    assert (pdom_nempty (pdom_bind f S)).
+    destruct x.
+    exists (bot Y).
+    simpl.
+    split.
+    apply pdom_neg_is_empty_nempty.
+    apply pdom_lift_non_empty_1.
+    intro.
+    apply (H1 _ e).
+    split.
+    intros.
+    destruct H1.
+    destruct (lem (pdom_nempty S0)); auto.
+    contradict H0.
+    destruct x.
+    destruct H1.
+    simpl in H1.
+    contradict (flat_bot_neq_total _ H1).
+    exists x; split; destruct H1; auto.
+    simpl in H1.
+    injection H1; intro.
+    rewrite H3.
+    intros i j.
+    contradict H2.
+    exists i; auto.
+    left.
+    split; auto.
+    exists (bot X); auto.
+
+    {
+      assert (exists y, y ∈ f x).
+      destruct (lem (exists y, y ∈ f x)); auto.
+      contradict H0.
+      exists x; split; auto.
+      intros i j.
+      contradict H1.
+      exists i; auto.
+      destruct H1.
+      unfold pdom_nempty.
+      exists x0.
+      simpl.
+      split.
+      apply pdom_neg_is_empty_nempty.
+      apply pdom_lift_non_empty_1.
+      intro.
+      apply (H2 _ e).
+      split.
+      intros.
+      destruct H2.
+      destruct (lem (pdom_nempty S0)); auto.
+      contradict H0.
+      destruct H2.
+      destruct x1.
+      simpl in H2.
+      contradict (flat_bot_neq_total _ H2).
+      exists x1; split; auto.
+      
+      simpl in H2.
+      injection H2; intro.
+      rewrite H4.
+      intros i j.
+      contradict H3.
+      exists i; auto.
+      right.
+      exists (f x).
+      split; auto.
+      exists (total x); auto.
+    }
+    
+    apply pdom_nempty_neg_is_empty in H1.
+    apply H1, H.
+  Defined.
+  
+  
+  Lemma pdom_bind_bot_1 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
+    ~ pdom_is_empty (pdom_bind f S) ->
+    (bot X ∈ S \/ exists x, total x ∈ S /\ bot Y ∈ f x) ->  
+    (bot Y) ∈ (pdom_bind f S).
+  Proof.
+    intros.
+    destruct H0.
+    split.
+    apply pdom_neg_is_empty_nempty.
+    apply pdom_lift_non_empty_1.
+    intro.
+    apply H.
+    apply pdom_bind_empty_1.
+    left; auto.
+    split.
+    intros.
+    apply pdom_neg_is_empty_nempty.
+    intro.
+    apply H.
+    apply pdom_bind_empty_1.
+    right; auto.
+    pose proof (pdom_lift_total_2 _ _ _ H1).
+    destruct H3 as [a [b c]].
+    exists a; split; auto.
+    rewrite <- c.
+    exact H2.
+    left.
+    split; auto.
+    simpl.
+    exists (bot X); split; simpl; auto.
+
+    destruct H0.
+    destruct H0.
+    simpl.
+    split.
+    apply pdom_neg_is_empty_nempty.
+    apply pdom_lift_non_empty_1.
+    intro.
+    apply H.
+    apply pdom_bind_empty_1.
+    left; auto.
+    split.
+    intros.
+    apply pdom_neg_is_empty_nempty.
+    intro.
+    contradict H.
+    apply pdom_bind_empty_1.
+    right.
+    destruct H2.
+    destruct H.
+    destruct x0.
+    simpl in H2.
+    contradict (flat_bot_neq_total _ H2).
+    simpl in H2.
+    exists x0.
+    split; auto.
+    injection H2; intro e; rewrite e; auto.
+    right.
+    exists (f x).
+    split; auto.
+    exists (total x); auto.
+  Defined.
+
+  Lemma pdom_bind_bot_2 {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
+    (bot Y) ∈ (pdom_bind f S) -> bot X ∈ S \/ exists x, total x ∈ S /\ bot Y ∈ f x.
+  Proof.
+    intros.
+    destruct H as [a [b c]].
+    destruct c.
+    destruct H.
+    left.
+    apply pdom_lift_bot_2 in H0; auto.
+    destruct H.
+    destruct H.
+    
+    right.
+    apply pdom_lift_total_2 in H0.
+    destruct H0 as [p [q r]]; exists p; split; auto.
+    rewrite <- r; auto.
+  Defined.
+  
+  Lemma pdom_bind_not_contain_bot {X Y : Type} (f : X -> pdom Y) (S : pdom X) :
+    ~ pdom_is_empty (pdom_bind f S) ->
+    ~ ((bot Y) ∈ pdom_bind f S) -> forall s, total s ∈ S -> ~ (bot Y ∈ f s).
+  Proof.
+    intros.
+    intro.
+    contradict H0.
+    apply pdom_bind_bot_1.
+    intro.
+    contradict (H H0).
+    right.
+    exists s; auto.
+  Defined.
+  
+  
 
   Lemma pdom_is_in_add_element_is_in {X : Type} (p : pdom X) (x y : flat X) : x ∈ p -> x ∈ pdom_add_elem p y.
   Proof.
@@ -1933,16 +2160,35 @@ Section PowerdomainContinuity.
     }
     {
       (* when ⊥ is not in f^† S *)
-      pose proof (pdom_bind_not_contain_bot _ _ H0).
-      assert (forall a, total a ∈ S -> f a = g a) as jj.
+      destruct (lem (pdom_is_empty (pdom_bind f S))).
       {
-        intros.
-        destruct (H a); auto.
-        destruct H3.
-        contradict (H1 _ H2 H3).
-      }      
-      left.
-      apply pdom_bind_agree; auto.
+        left.
+        rewrite (pdom_is_empty_is_empty _ H1).
+        apply eq_sym, pdom_is_empty_is_empty.
+        apply pdom_bind_empty_1.
+        destruct (pdom_bind_empty_2 _ _ H1).
+        left; auto.
+        right.
+        destruct H2 as [a [b c]].
+        exists a; split; auto.
+        destruct (H a).
+        rewrite <- H2; auto.
+        destruct H2.
+        contradict (c _ H2).
+      }
+      {      
+        pose proof (pdom_bind_not_contain_bot _ _ H1 H0).
+        assert (forall a, total a ∈ S -> f a = g a) as jj.
+        {
+          intros.
+          destruct (H a); auto.
+          destruct H4.
+          contradict (H2 _ H3 H4).
+        }      
+        left.
+        apply pdom_bind_agree; auto.
+      }
+
     }
   Defined.
   
